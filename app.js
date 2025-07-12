6e01df9be5a41b7cbb3a389030119b42cc850cf1
Aman Kumar <amankumarmaurya93@gmail.com>
2025-07-12 11:08:30 +0530

    Server Print new mssage

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var http = require('http')
 http.createServer(function(req, res){
     
     res.writeHead(200, {'content-Type': 'text/html'})
-    res.end("Welcome back aliens to the server!")
+    res.end("Welcome back Aliens from outer space!")
 }).listen(8080)
